e7018b376ea548221d0df203f8894cf2628d66a8
arinapras <116898611+arinapras@users.noreply.github.com>
2024-01-16 16:54:36 +0800

    Add project files.

---
+++ PE07_grp4_Project/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PE07_grp4_Project.Server.Models;
+
+namespace PE07_grp4_Project.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ PE07_grp4_Project/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PE07_grp4_Project.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
